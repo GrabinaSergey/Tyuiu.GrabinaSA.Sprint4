22da4750cf4ff1d134958ef331cc10f281d54154
GrabinaSergey <dieumortx@gmail.com>
2024-11-12 18:56:11 +0500

    Task5

---
+++ Tyuiu.GrabinaSA.Sprint4.Task5.V13.Test/DataServiceTest.cs
@@ -0,0 +1,27 @@
+using Tyuiu.GrabinaSA.Sprint4.Task5.V13.Lib;
+namespace Tyuiu.GrabinaSA.Sprint4.Task5.V13.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int[,] matrix = new int[5, 5] { { 0, 0, 0, 0, -9 },
+                                            { -7, 0, 0, 0, -9 },
+                                            { 0, -7, -9, 0, -5 },
+                                            { -7, -5, -7, -7, 0 },
+                                            { 0, 0, 0, -7, -9 } };
+            int[,] res = ds.Calculate(matrix);
+
+            int[,] wait = new int[5, 5] { { 0, 0, 0, 0, 0 },
+                                          { 0, 0, 0, 0, 0 },
+                                          { 0, 0, 0, 0, 0 },
+                                          { 0, 0, 0, 0, 0 },
+                                          { 0, 0, 0, 0, 0 } }; ;
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.GrabinaSA.Sprint4.Task5.V13/Program.cs
@@ -0,0 +1,48 @@
+﻿using Tyuiu.GrabinaSA.Sprint4.Task5.V13.Lib;
+namespace Tyuiu.GrabinaSA.Sprint4.Task5.V13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Random rnd = new Random();
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                           *");
+            Console.WriteLine("******************************************************************************");
+
+            Console.WriteLine("Введите коллво строк в массиве : ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите коллво столбцов в массиве : ");
+            int col = Convert.ToInt32(Console.ReadLine());
+
+
+            int[,] mtrx = new int[rows, col];
+
+            Console.WriteLine("******************************************************************************");
+
+            for (int i = 0; i < rows; i++)
+                for (int j = 0; j < rows; j++)
+                {
+                    mtrx[i, j] = rnd.Next(-2, 5);
+                }
+
+            Console.WriteLine("\nМассив");
+            for (int i = 0; i < rows; i++)
+                for (int j = 0; j < col; j++)
+                {
+                    Console.WriteLine($"{mtrx[i, j]} \t");
+                }
+            Console.WriteLine();
+
+            Console.WriteLine();
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* Результат:                                                                 *");
+            Console.WriteLine("******************************************************************************");
+
+            Console.WriteLine("zxc " + ds.Calculate(mtrx));
+            Console.ReadKey();
+        }
+    }
+}
