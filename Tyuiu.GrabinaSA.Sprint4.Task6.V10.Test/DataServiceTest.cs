8f3708c7c9a58df00a3608109fa789d8db89e6cc
GrabinaSergey <dieumortx@gmail.com>
2024-11-12 19:08:54 +0500

    Task6

---
+++ Tyuiu.GrabinaSA.Sprint4.Task6.V10.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.GrabinaSA.Sprint4.Task6.V10.Lib;
+namespace Tyuiu.GrabinaSA.Sprint4.Task6.V10.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string[] i = { "Òåàòð", "Êèíî", "Ìóçåé", "Ïàðê", "Çîîïàðê", "Êîíöåðò", "Âûñòàâêà" };
+            string[] j = ds.Calculate(i);
+            string[] k = { "Òåàòð", "Êèíî", "Ìóçåé", "Ïàðê" };
+            CollectionAssert.AreEqual(k, j);
+        }
+    }
+}
---
+++ Tyuiu.GrabinaSA.Sprint4.Task6.V10/Program.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.GrabinaSA.Sprint4.Task6.V10.Lib;
+namespace Tyuiu.GrabinaSA.Sprint4.Task6.V10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ:                                                            *");
+            Console.WriteLine("*                                                                            *");
+            string[] i = { "Театр", "Кино", "Музей", "Парк", "Зоопарк", "Концерт", "Выставка" };
+            for (int j = 0; j < i.Length; j++) { Console.Write($"{i[j]}\t"); }
+            Console.WriteLine();
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("*РЕЗУЛЬТАТ:                                                                  *");
+            Console.WriteLine("******************************************************************************");
+            string[] res = ds.Calculate(i);
+            for (int j = 0; j < res.Length; j++) { Console.Write($"{res[j]}\t"); }
+            Console.WriteLine();
+        }
+    }
+}
